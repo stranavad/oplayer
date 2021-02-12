97056e80123314eac231a18ba2cfba7d310da1e3
stranavad <stranava.david@gmail.com>
2021-02-12 12:12:33 +0100

    Test.py organize

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-import os
-
-for x in os.listdir('.'):
-    if os.path.isfile(x): print('f-' + x)
-    elif os.path.isdir(x): print('d-' + x)
-    elif os.path.islink(x): print('l-' + x)
-    else: print('---' + x)
+# File for function testing
